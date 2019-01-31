3855c694c3b7ba08b2a9ee4f9a663f19482975ff
cj162 <cj162@duke.edu>
2019-01-30 20:17:33 -0500

    update

--- login/models.py
+++ login/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from enum import Enum
 # Create your models here.
 
 
 class User(AbstractUser):
-    pass
+    class Roles(Enum):
+        OW = 'owner'
+        DR = 'driver'
+        SH = 'sharer'
+
+    catalog = models.CharField(max_length=6, choices=[(catalog, catalog.value) for catalog in Roles])
+    plate_number = models.CharField(max_length=10, blank=True)
